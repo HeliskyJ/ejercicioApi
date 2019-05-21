de97424ff4a51cfe609f488897d1e1bd3baf501e
Helisky <skhely998@gmail.com>
2019-05-21 01:08:09 -0600

    Incluye diseño para FIPE API

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var ul;
 var a;
 
 
+
 function valie(){
 var un = $(".select").val();
 ul = (apis +un+"/marcas");
@@ -19,20 +20,22 @@ ul = (apis +un+"/marcas");
                 $("#table").empty();
                 $(".bano").empty();
                 $(".ban").empty();
+                $(".bon").empty();
                 $(".bin").empty();
-                $("#table").append("<table border='2'>"+
+                $("#o").empty();
+                $("#table").append("<table border='2' class='table is-bordered is-striped is-narrow is-hoverable is-fullwidth tblData'>"+
                 "<thead>"+
-                "<th>Nombre</th>"+
-                "<th>Codigo</th>"+
+                "<th scope='col'>Nombre</th>"+
+                "<th scope='col'>Codigo</th>"+
                 "</thead>"+
                 "<tbody>");
                 $.each(data, function(i, item) {
                             $("#table table").append("<tr>" +
                               "<td>" + item.nome + "</td>" +
                               "<td>" + item.codigo + "</td>" +
-                              "<td id='bonton'><button value='"+item.codigo+
+                              "<td id='bonton'><button class='button is-warning is-focused' value='"+item.codigo+
                               "'onclick='mo(this.value)' id='modelo'>"+
-                              "Esta marca</button></td>" +
+                              "Modelos</button></td>" +
                               "</tr></tbody></table>");
                 });
             },
@@ -54,11 +57,12 @@ ul = ul+"/"+a+"/modelos";
             success: function (data){
                 $(".bin").empty();
                 $("#tabTwo").empty();
+                $(".bon").empty();
                 $("#tabOne").empty();
                 $("#table").empty();
                 cont=0;
                 $(".bano").append("<h1>Modelos</h1>"+
-                "<table border='2' id='tabTwo'>"+
+                "<table border='2' id='tabTwo' class='table is-bordered is-striped is-narrow is-hoverable is-fullwidth'>"+
                 "<thead>"+
                 "<th>Nombre</th>"+
                 "<th>Codigo</th>"+
@@ -68,7 +72,7 @@ ul = ul+"/"+a+"/modelos";
                     $(".bano table").append("<tr>"+
                     "<td>" + data.modelos[cont].nome + "</td>"+
                     "<td>" + data.modelos[cont].codigo + "</td>"+
-                    "<td id='bonton'><button value='"+data.modelos[cont].codigo+
+                    "<td id='bonton'><button class='button is-warning is-focused' value='"+data.modelos[cont].codigo+
                     "'onclick='act(this.value)' id='but'>"+
                     "Año</button></td>" +
                     "</tr></tbody></table>");
@@ -76,7 +80,7 @@ ul = ul+"/"+a+"/modelos";
                 }
                 sum=0;
                 $(".ban").append("<h1>Años</h1>"+
-                "<table border='2' id='tabOne'>"+
+                "<table border='2' id='tabOne' class='table is-bordered is-striped is-narrow is-hoverable is-fullwidth'>"+
                 "<thead>"+
                 "<th>Nombre</th>"+
                 "<th>Codigo</th>"+
@@ -110,7 +114,8 @@ ul = ul+"/"+a+"/anos";
             $("#tabTwo").empty();
             $(".bano").empty();
             $(".ban").empty();
-            $(".bin").append("<table border='2' id='tabTrh'>"+
+            $(".bon").empty();
+            $(".bin").append("<table border='2' id='tabTrh' class='table is-striped is-narrow is-hoverable is-fullwidth'>"+
             "<thead>"+
             "<th>Nombre</th>"+
             "<th>Codigo</th>"+
@@ -120,11 +125,43 @@ ul = ul+"/"+a+"/anos";
                         $(".bin table").append("<tr>" +
                           "<td>" + item.nome + "</td>" +
                           "<td>" + item.codigo + "</td>" +
-                          "<td id='bonton'><button value='"+item.codigo+
+                          "<td id='bonton'><button class='button is-warning is-focused' value='"+item.codigo+
                           "'onclick='mart(this.value)' id='mrt'>"+
                           "Valor</button></td>" +
                           "</tr></tbody></table>");
             });
+        },
+        })
+    })
+}
+
+function mart(valor){
+a = valor;
+ul = ul+'/'+a;
+
+    $('#table').ready(function(){
+        $.ajax({
+            url: ul,
+            type:"GET",
+            dataType: "json",
+            success: function (data){
+            $(".bin").empty();
+            $("#tabOne").empty();
+            $("#tabTwo").empty();
+            $(".bano").empty();
+            $(".ban").empty();
+            $(".bon").empty();
+                        $(".bon").append("<div class='box pn' id='b'><ul>" +
+                          "<li>"+ "<b>Valor</b> " + data.Valor + "</li>" +
+                          "<li>"+ "<b>Marca</b> " + data.Marca+ "</li>" +
+                          "<li>"+ "<b>Modelo</b> " + data.Modelo+ "</li>" +
+                          "<li>"+ "<b>Año del modelo</b> " + data.AnoModelo+ "</li>" +
+                          "<li>"+ "<b>Combustible</b> " + data.Combustivel+ "</li>" +
+                          "<li>"+ "<b>Codigo FIPE</b> " + data.CodigoFipe+ "</li>" +
+                          "<li>"+ "<b>Tipo de vehículo</b> " + data.TipoVeiculo+ "</li>" +
+                          "<li>"+ "<b>Sigla Combustible</b> " + data.SiglaCombustivel+ "</li>" +
+                          "<li>"+ "<b>Mes de referencia</b> " + data.MesReferencia+ "</li>" +
+                          "</ul></div>");
         },
         })
     })
